76102984230384608b5c65fc227a9ec3d2a5183e
JhonatanBuezo <107516036+JhonatanBuezo@users.noreply.github.com>
2022-07-08 07:48:45 -0300

    initial commit

---
+++ cdg.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    var onload = document.getElementById('loader');
+
+    onload.style.visibility = 'hidden';
+    onload.style.opacity ='0';
+}
+window.onload = function(){
+    $('#onload').fadeOut();
+    $('body').removeClass('hidden');
+}
